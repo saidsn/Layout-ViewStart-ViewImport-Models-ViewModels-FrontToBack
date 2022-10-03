da2f7249d64375a128300274a0d3f463cc3a515c
saidsn <saidsn@code.edu.az>
2022-10-03 21:15:38 +0400

    Homework

---
+++ ClassCode/ClassCode/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using ClassCode.Models;
+using ClassCode.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+         
+
+            List<Book> books = new List<Book>();
+            Car car = new Car
+            {
+                Id = 1,
+                Name = "BMV",
+                Color = "Black"
+            };
+            Book book1 = new Book
+            {
+                Id = 1,
+                Name = "Yeddi Gozel",
+                AuthorName = "Nizami",
+                PublishDate = new DateTime(1500,5,1)
+            };
+            Book book2 = new Book
+            {
+                Id = 2,
+                Name = "Leyli Mecnun",
+                AuthorName = "Mehhemed Fizuli",
+                PublishDate = new DateTime(1500, 5, 1)
+            };
+            Book book3 = new Book
+            {
+                Id = 3,
+                Name = "Koc",
+                AuthorName = "Cefer Cabbarli",
+                PublishDate = new DateTime(1500, 5, 1)
+            };
+
+            books.Add(book1);
+            books.Add(book2);
+            books.Add(book3);
+
+            HomeVM model = new HomeVM
+            {
+                Books= books,
+                Car = car
+            };
+
+            return View(model);
+        }
+
+     
+
+      
+    }   
+
+}
---
+++ ClassCode/ClassCode/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassCode.Models
+{
+    public class Book:BaseEntity
+    {
+      
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
